4d1b9bec16ed486a30e5a922c82eea7235a88b33
Dan <JamesDanielBaird@gmail.com>
2020-01-12 13:47:15 -0800

    Add another

---
+++ leetcode/209/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        # I assume they are looking for the ascii offset trick for letters above a certain value
+        return str.lower()
